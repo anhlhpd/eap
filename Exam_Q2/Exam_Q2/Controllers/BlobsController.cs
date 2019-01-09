f7f1788f29615438cd841f46c5b691b48a72016d
anhlhpd00579 <anhlhpd00579@fpt.edu.vn>
2019-01-09 23:58:03 +0700

    Q2

---
+++ Exam_Q2/Exam_Q2/Controllers/BlobsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Exam_Q2.Controllers
+{
+    public class BlobsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //private CloudBlobContainer GetCloudBlobContainer()
+        //{
+        //    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+        //            CloudConfigurationManager.GetSetting("<storageaccountname>_AzureStorageConnectionString"));
+        //    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+        //    CloudBlobContainer container = blobClient.GetContainerReference("test-blob-container");
+        //    return container;
+        //}
+    }
+}
---
+++ Exam_Q2/Exam_Q2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+
+namespace Exam_Q2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
